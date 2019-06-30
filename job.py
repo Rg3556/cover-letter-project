883f884fb5808a6ae907ab02624894fbbbc82812
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-30 12:51:17 -0400

    job format setup

--- job.py
+++ job.py
@@ -4,6 +4,11 @@ from selenium import webdriver
 # and replace the path with where you download it
 driver = webdriver.Chrome('E:\Python scripts\chromedriver')
 
-driver.get('https://www.linkedin.com/jobs/view/1304372932/')
+
+print("Welcome to Cover Letter Premium!")
+print("------------------------------")
+job_url = input("please input your URL: ")
+# breakpoint()
+driver.get(job_url)
 
 print(driver.find_element_by_css_selector("div.description__text--rich").text)
